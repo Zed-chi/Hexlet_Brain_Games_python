db7f314d96a524c508188252c6862366b7da0a74
zedchi <zed-chi@mail.ru>
2019-09-25 22:31:28 +0300

    new logic

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,8 @@
+from brain_games.cli import run
+
 def main():
-    print("Welcome to the Brain Games")
-
-
+    print("Welcome to the Brain Games!")
+    run()
+    
 if __name__ == "__main__":
     main()
