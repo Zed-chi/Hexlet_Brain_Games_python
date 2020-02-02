3668475aae4017bd4aaeffb79563bb77a5f7e33b
zed <zed@mail.ru>
2020-02-02 22:44:12 +0300

    added prime game

---
+++ brain_games/scripts/games/brain_prime.py
@@ -0,0 +1,34 @@
+from random import randint
+import prompt
+
+
+def is_num_prime(num):
+    for i in range(2, 10):
+        if num % 2 == 0:
+            return False
+    return True
+
+
+def prime_game():
+    num = randint(1, 50)
+    correct_answer = "yes" if is_num_prime(num) else "no"
+    print(f"Question: {num}")
+    answer = prompt.string(prompt="Your answer: ", empty=False)
+    if answer == correct_answer:
+        print("Correct!")
+        return True
+    else:
+        print(
+            f"'{answer}' is wrong answer ;(. ",
+            f"Correct answer was '{correct_answer}'.",
+        )
+        return False
+
+
+game = {
+    "title": "Prime number game",
+    "description":
+        "Answer 'yes' if given number is prime. " +
+        "Otherwise answer 'no'.\n",
+    "game": prime_game,
+}
