230f4c95b8d6bbb77c2582a05b0a5c6214bc799d
zed <zed@mail.ru>
2020-02-02 20:54:42 +0300

    added gcd game to main module

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,35 @@
 from ..cli import welcome_user
+from .games.brain_calc import game as calc
+from .games.brain_even import game as even
+from .games.brain_gcd import game as gcd
+import prompt
+
+
+GAMES = [calc, even, gcd]
+ANSWERS_TO_WIN = 3
 
 
 def main():
-    print("Welcome to the Brain Games!\n")
-    welcome_user()
+    print("Welcome to the Brain Games!")
+    name = welcome_user()
+    print("\n")
+    print("Games: ")
+    for i, v in enumerate(GAMES):
+        print(f"{i}. {v['title']}")
+
+    choice = None
+    while choice == None or choice > len(GAMES) - 1 or choice < 0:
+        choice = prompt.integer(prompt="Your choice: ")
+    game = GAMES[choice]
+
+    print(game["description"])
+    correct_answers = 0
+
+    while correct_answers < ANSWERS_TO_WIN:
+        if game["game"]():
+            correct_answers += 1
+    print(f"Congratulations, {name}!")
+    return None
 
 
 if __name__ == "__main__":
