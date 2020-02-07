5d631e0eb49b8b229f8db4be62786c3b236e008d
zed <zed@mail.ru>
2020-02-07 20:14:56 +0300

    zero div fix

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,13 +6,15 @@ RULES = "What is the result of the expression?"
 
 
 def game_round():
-    num_a = randint(0, 100)
-    num_b = randint(0, 100)
+    num_a = randint(1, 100)
+    num_b = randint(1, 100)
     operation_char, operation = choice(
-        ("+", int.__add__),
-        ("-", int.__sub__),
-        ("*", int.__mul__),
-        ("//", int.__floordiv__),
+        (
+            ("+", int.__add__),
+            ("-", int.__sub__),
+            ("*", int.__mul__),
+            ("//", int.__floordiv__)
+        )
     )
     question = f"Question: {num_a} {operation_char} {num_b}"
     result = operation(num_a, num_b)
