4462b0c61bd354d2dc731609fddbfc2c9f0a623d
zed <zed@mail.ru>
2020-02-07 20:15:13 +0300

    func fix

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,8 @@
 from brain_games.games import calc, even, prime, progression, gcd
 from brain_games import engine
-from brain_games import game_chooser
+from brain_games.game_chooser import choose_game
 
 
 def main():
-    game = game_chooser(games=[calc, even, prime, progression, gcd])
+    game = choose_game(games=[calc, even, prime, progression, gcd])
     engine.run(game=game)
