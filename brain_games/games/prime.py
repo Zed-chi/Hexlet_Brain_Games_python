f16f62579617fc57d8d5823a572ca8cfc8bc4fe3
Zed-chi <zed-chi@mail.ru>
2020-02-10 15:31:13 +0300

    changed boundary of prime calculation

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,4 +1,5 @@
 from random import randint
+from math import sqrt
 
 
 TITLE = "Prime number game"
@@ -6,18 +7,14 @@ RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'."
 
 
 def is_num_prime(num):
-    if num < 10:
-        end = num
-    else:
-        end = 10
-    for i in range(2, end):
+    for i in range(2, sqrt(num)):
         if num % i == 0:
             return False
     return True
 
 
 def game_round():
-    num = randint(1, 50)
+    num = randint(2, 50)
     correct_answer = "yes" if is_num_prime(num) else "no"
     question = f"Question: {num}"
     return (question, correct_answer)
