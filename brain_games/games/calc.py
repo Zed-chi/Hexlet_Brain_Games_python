8101cadc7e5a72b5d9fe4fe216db398ff0bbf4a1
Zed-chi <zed-chi@mail.ru>
2020-02-10 15:27:36 +0300

    moved operations to const

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,19 +3,18 @@ from random import randint, choice
 
 TITLE = "Calculation game"
 RULES = "What is the result of the expression?"
+OPERATIONS = (
+    ("+", int.__add__),
+    ("-", int.__sub__),
+    ("*", int.__mul__),
+    ("//", int.__floordiv__)
+)
 
 
 def game_round():
     num_a = randint(1, 100)
     num_b = randint(1, 100)
-    operation_char, operation = choice(
-        (
-            ("+", int.__add__),
-            ("-", int.__sub__),
-            ("*", int.__mul__),
-            ("//", int.__floordiv__)
-        )
-    )
+    operation_char, operation = choice(OPERATIONS)
     question = f"Question: {num_a} {operation_char} {num_b}"
     result = operation(num_a, num_b)
     return (question, result)
