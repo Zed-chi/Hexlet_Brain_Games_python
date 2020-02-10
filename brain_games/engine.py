3bebb2209c82657bb378cb32f0bd7dc6456a2546
Zed-chi <zed-chi@mail.ru>
2020-02-10 15:21:56 +0300

    some if change

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -2,27 +2,27 @@ import prompt
 
 
 def run(game=None, answers_to_win=3):
-    if game is None:
-        return
-    print("Welcome to the Brain Games!")
-    print(game.RULES, "\n")
+    if game:
+        print("Welcome to the Brain Games!")
+        print(game.RULES)
+        print()
 
-    name = prompt.string(empty=False, prompt="May I have your name? ")
-    print(f"Hello {name}\n")
+        name = prompt.string(empty=False, prompt="May I have your name? ")
+        print(f"Hello {name}\n")
 
-    correct_answers = 0
-    while correct_answers < answers_to_win:
-        question, correct_answer = game.game_round()
-        print(question)
-        user_answer = prompt.string(prompt="Your Answer: ")
-        if user_answer == str(correct_answer):
-            print("Correct!")
-            correct_answers += 1
-        else:
-            print(
-                f"'{user_answer}' is wrong answer ;(. ",
-                f"Correct answer was '{correct_answer}'.",
-            )
-            print(f"Let's try again, {name}!")
-            return
-    print(f"Congratulations, {name}!")
+        correct_answers = 0
+        while correct_answers < answers_to_win:
+            question, correct_answer = game.game_round()
+            print(question)
+            user_answer = prompt.string(prompt="Your Answer: ")
+            if user_answer == str(correct_answer):
+                print("Correct!")
+                correct_answers += 1
+            else:
+                print(
+                    f"'{user_answer}' is wrong answer ;(. ",
+                    f"Correct answer was '{correct_answer}'.",
+                )
+                print(f"Let's try again, {name}!")
+                return
+        print(f"Congratulations, {name}!")
